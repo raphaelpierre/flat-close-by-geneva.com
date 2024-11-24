19017457a848bf5baa3a6a9f34be542c6dd65192
raphaelpierre <pierre.raphael@gmail.com>
2024-11-24 21:14:20 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,9 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/flat-close-by-geneva.com/'
+  
   plugins: [react()],
+  base: '/flat-close-by-geneva.com/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
