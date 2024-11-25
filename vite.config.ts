e2125ce5a0ac0cc9dfaf8457c9db62fd47f9dbe0
Raphaël Pierre <pierre.raphael@gmail.com>
2024-11-25 07:30:18 +0100

    modif base in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/flat-close-by-geneva.com/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
